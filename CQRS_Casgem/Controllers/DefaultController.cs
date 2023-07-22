33846bd8ef4a1220e9c8800bc1aa0c1db1319639
MuratYucedag <yucedagmurat23@gmail.com>
2023-07-22 12:16:06 +0300

    Add project files.

---
+++ CQRS_Casgem/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using CQRS_Casgem.CQRSPattern.Handlers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CQRS_Casgem.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly GetProductQueryHandler _getProductQueryHandler;
+        public DefaultController(GetProductQueryHandler getProductQueryHandler)
+        {
+            _getProductQueryHandler = getProductQueryHandler;
+        }
+        public IActionResult Index()
+        {
+            var values = _getProductQueryHandler.Handle();
+            return View(values);
+        }
+    }
+}
